15a338e5cfb9009fef47aa133aedbf32375a5c3a
Ryan Jensen <jensenr30@gmail.com>
2025-12-10 20:39:58 -0600

    Working on cmake

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-PARALLEL_JOBS=$(nproc)
-DEBUG_CXX_FLAGS=
-CMAKE_OPTIONS=
-
 mkdir -p build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -B build -S .
-cmake --build build --target particle_sim -- -j $(PARALLEL_JOBS)
+cmake --build build --target particle_sim -- -j $(nproc)
